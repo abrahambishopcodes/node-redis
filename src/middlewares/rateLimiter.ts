972afa8dd58163c6a502381492b91ffbc2d3d504
abrahambishopcodes <crypticcodetechnologies@gmail.com>
2026-05-07 17:26:51 +0100

    refactor: optimize rate limiter logic to use atomic increment and conditional expiration

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -6,25 +6,20 @@ export const rateLimiter = async (req: Request, res: Response, next: NextFunctio
     const key = `requests:${req.ip}`;
     const limit = Number(getFromEnv("API_RATE_LIMIT_PER_MIN"));
     
-    // Get the request count and ttl
-    const requestCount = await redisClient.get(key);
-    const ttl = await redisClient.ttl(key);
+    // Increment the request count
+    const requestCount = await redisClient.incr(key);
 
-    if (requestCount) {
-        // Check if the request count has exceeded the api limit
-        if (Number(requestCount) >= limit) {
-            return res.status(429).json({
-                message: `Too many requests, try again in ${ttl} seconds`,
-            })
-        }
-
-        // increment the req count if not exceeded
-        await redisClient.incr(key);
-        next();
-        return;
+    if (requestCount == 1) {
+        await redisClient.expire(key, 60);
     }
 
-    // set the request count if not found
-    await redisClient.setEx(key, 60, "1");
+    if (requestCount > limit) {
+        const ttl = await redisClient.ttl(key);
+        return res.status(429).json({
+            message: `Too many requests, try again in ${ttl} seconds`,
+        })
+    }
+
     next();
+    
 }
